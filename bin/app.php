cceb77efe0bc0d50c644dbc62b25be8193e60a77
shamsuddinov <shamsuddinova@gmail.com>
2023-12-23 18:25:09 +0500

    Doctrine bilan ishlash qo'shildi

--- bin/app.php
+++ bin/app.php
@@ -1,6 +1,9 @@
 #!/usr/bin/env php
 <?php
 
+use Doctrine\DBAL\Migrations\Tools\Console\Helper\ConfigurationHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
 
@@ -14,6 +17,18 @@ $container = require 'config/container.php';
 
 $cli = new Application();
 $commands = $container->get('config')['console']['commands'];
+
+$entityManager = $container->get(EntityManagerInterface::class);
+$connection = $entityManager->getConnection();
+
+$configuration = new Doctrine\DBAL\Migrations\Configuration\Configuration($connection);
+$configuration->setMigrationsDirectory('db/migrations');
+$configuration->setMigrationsNamespace('Migration');
+
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+$cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
+
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
 
 foreach ($commands as $command){
     $cli->add($container->get($command));
--- config/autoload/console.global.php
+++ config/autoload/console.global.php
@@ -1,23 +1,25 @@
 <?php
 
 use App\Console\Command\CacheClearCommand;
-use App\Service\FileManager;
-use Psr\Container\ContainerInterface;
 
 return [
     'dependencies' => [
         'factories' => [
-            CacheClearCommand::class => function (ContainerInterface $container) {
-                return new CacheClearCommand(
-                    $container->get('config')['console']['cachePaths'],
-                    $container->get(FileManager::class)
-                );
-            },
+            CacheClearCommand::class => Infrastructure\App\Console\Command\CacheClearCommandFactory::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand::class => Infrastructure\App\Doctrine\Factory\DiffCommandFactory::class,
         ],
     ],
     'console' => [
         'commands' => [
-            CacheClearCommand::class
+            CacheClearCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\ExecuteCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\LatestCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\MigrateCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\DiffCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\UpToDateCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\StatusCommand::class,
+            Doctrine\DBAL\Migrations\Tools\Console\Command\VersionCommand::class,
         ],
         'cachePaths' => [
             'twig' => 'var/cache/twig',
